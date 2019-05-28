c7c04020e579d5813002f6517c8262dd0586f1e4
nikolas <nikoagge@gmail.com>
2019-05-28 09:24:05 +0300

    Initial Commit

---
+++ ReceiptsDiaryTabbedApp 1.0/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ReceiptsDiaryTabbedApp 1.0
+//
+//  Created by Nikolas on 28/05/2019.
+//  Copyright © 2019 Nikolas Aggelidis. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
